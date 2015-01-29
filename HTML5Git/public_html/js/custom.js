63032dcd19b581349e613fd47e5ede1c4c183f28
iproduct <office@iproduct.org>
2015-01-29 14:21:04 +0200

    JS example added.

---
+++ HTML5Git/public_html/js/custom.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function showInfo() {
+    document.getElementById("display").innerHTML = "<b>Hello from JavaScript!</b>";
+}
+
+
+function changeSrc(){
+    document.getElementById("picture_html").src = "img/html.png";
+}
+
+function changeBack(){
+    document.getElementById("picture_html").src = "img/HTML5_logo.png";
+}
