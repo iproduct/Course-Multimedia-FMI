d1c8faadf53796661d48d5f71ba08aa4143c48b9
iproduct <office@iproduct.org>
2015-04-03 18:53:26 +0200

    javascript object-oriented demo - CAD System initial commit.

--- MMT_Canavas_Demo/public_html/js/cad_system.js
+++ MMT_Canavas_Demo/public_html/js/cad_system.js
@@ -105,22 +105,3 @@ Ellipse.prototype.draw = function (context) {
     context.fill();
     context.stroke();
 };
-
-function drawEllipse(ctx, x, y, w, h) {
-    var kappa = .5522848,
-            rx = (w / 2) * kappa, // control point offset horizontal
-            ry = (h / 2) * kappa, // control point offset vertical
-            xe = x + w, // x-end
-            ye = y + h, // y-end
-            xm = x + w / 2, // x-middle
-            ym = y + h / 2;       // y-middle
-
-    ctx.beginPath();
-    ctx.moveTo(x, ym);
-    ctx.bezierCurveTo(x, ym - oy, xm - ox, y, xm, y);
-    ctx.bezierCurveTo(xm + ox, y, xe, ym - oy, xe, ym);
-    ctx.bezierCurveTo(xe, ym + oy, xm + ox, ye, xm, ye);
-    ctx.bezierCurveTo(xm - ox, ye, x, ym + oy, x, ym);
-
-    ctx.stroke();
-}
