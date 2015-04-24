b8e478d8e759b087778949a31c6371147a920e0a
iproduct <office@iproduct.org>
2015-04-24 16:16:44 +0200

    javascript jQuery basic demo - initial commit

---
+++ MMT_jQuery/public_html/js/demo01.js
@@ -0,0 +1,9 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+jQuery.noConflict();
+jQuery(function($){
+    $("div.demo1").css("border", "1px solid green");
+});
