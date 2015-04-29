df5ac7f51486e6e351def9a9a8e2843fd98fa0fb
iproduct <office@iproduct.org>
2015-04-29 14:08:41 +0200

    MMT_jQuery phone click listener added.

--- MMT_jQuery/public_html/js/ajax.js
+++ MMT_jQuery/public_html/js/ajax.js
@@ -12,14 +12,20 @@ jQuery(function ($) {
         })
         .done(function (phonesData) {
             console.log("Data:", phonesData);
+            $( "#phones tbody" ).empty();
             $.each(phonesData, function(index, phone) {
-                $( "#phones tbody" ).append( "<tr>" +
+                $( "#phones tbody" ).append( "<tr data-id='" +
+                        phone.id + "'>" +
                     "<td>" + index + "</td>" +
                     "<td><img src='" + phone.imageUrl + 
                     "' alt='" + phone.name + " picture'></td>" +
                     "<td>" + phone.name + "</td>" +
                     "<td>" + phone.snippet + "</td>" +
                   "</tr>" );
+            });
+            // add click listeners to all phones
+            $("#phones tr").click(function(){
+                console.log($(this).attr("data-id"));
             });
         })
         .fail(function () {
@@ -30,5 +36,29 @@ jQuery(function ($) {
     loadPhonesList();
     $("#refresh_phones").click(function() {
         loadPhonesList();
-    })
+    });
+    
+    dialog = $( "#phone_details" ).dialog({
+        autoOpen: false,
+        height: 800,
+        width: 600,
+        modal: false,
+        buttons: {
+          "Add to Cart": function(){ console.log("Phone added to cart");},
+          Cancel: function() {
+            dialog.dialog( "close" );
+          }
+        },
+        close: function() {
+//          form[ 0 ].reset();
+//          allFields.removeClass( "ui-state-error" );
+        }
+    });
+    
+    
+    
+    
+    function showPhoneDetailsDialog(phoneId){
+    
+    }
 });
