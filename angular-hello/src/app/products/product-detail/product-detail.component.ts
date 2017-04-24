d95670ccc8a07b9794212c78247b619ba1b3d4b2
iproduct <office@iproduct.org>
2017-04-24 13:34:32 +0300

    template driven form added

---
+++ angular-hello/src/app/products/product-detail/product-detail.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms/src/forms';
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+  product: Product = new Product(undefined, undefined, undefined);
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(f: NgForm) {
+    console.log(f.value);
+  }
+
+}
