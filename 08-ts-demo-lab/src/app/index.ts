9015ea8348b7deb5c52f0f1df44358af341032e3
Trayan Iliev <office@iproduct.org>
2020-03-17 15:28:25 +0200

    project renaming and cleanup

--- 08-ts-demo-lab/src/app/index.ts
+++
@@ -1,7 +0,0 @@
-import { Customer, Admin } from './users';
-
-const bill = new Customer('Bill', 'Smith', 'bill@gmail.com', 'bill');
-const ana = new Admin('Ana', 'Georgieva', 'ana@gmail.com', 'ana');
-document.getElementById('content').innerHTML = 
-    `${bill.congratulation} <br>
-     ${ana.congratulation}`;
--- 08-ts-demo/src/greeter.ts
+++
@@ -1,5 +0,0 @@
-function greeter(person: string) {
-    return `Hello, ${person} from TypeScript!`;
-}
-const user = 'TypeScript User';
-document.body.innerHTML = greeter(user);
