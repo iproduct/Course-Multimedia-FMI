8a70a5713044a40f27ae1baf2fd757763855ba3f
Trayan Iliev <office@iproduct.org>
2021-03-26 19:29:53 +0200

    catch demo added

--- 04-es6-novelties-lab2/github-fetch-promises.js
+++ 04-es6-novelties-lab2/github-fetch-promises.js
@@ -2,12 +2,18 @@ function demo() {
     const resultsElem = document.getElementById('results');
     return fetch('users.json')
         .then(userResp => userResp.json())
-        .then(users => Promise.all(
-            users.map(user => fetch(`https://api.github.com/users/${user.name}`)
-                .then(gitUserResp => gitUserResp.json())
-            ))
-        ).then(gitUsers => 
-            gitUsers.map(gitUser => {
+        .then(users =>
+            Promise.all(
+                users.map(user => fetch(`https://api.github.com/users/${user.name}`)
+                    .then(gitUserResp => gitUserResp.json())
+                    .then(gitUser => gitUser.message === "Not Found" ? 
+                        Promise.reject(`GitHub user '${user.name}' not found.`):
+                        gitUser
+                    )
+                ))
+        ).then(gitUsers => {
+            console.log(gitUsers);
+            return gitUsers.map(gitUser => {
                 const img = new Image();
                 img.src = gitUser.avatar_url;
                 return img;
@@ -15,12 +21,12 @@ function demo() {
                 resultsElem.append(userImage);
                 return userImage;
             })
-        ).then(images => new Promise((resolve, reject) =>
+        }).then(images => new Promise((resolve, reject) =>
             setTimeout(resolve, 6000, images)
         )).then(images => {
             images.forEach(image => image.remove());
             return 'Demo finished';
-        });
+        }).catch(err => `Error fetching Git users: ${err}`);
 }
 
 window.addEventListener('load', () => demo().then(result => console.log(result)));
