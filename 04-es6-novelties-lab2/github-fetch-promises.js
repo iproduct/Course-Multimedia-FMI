fb09ed55c604e704b89983de681d84569a190374
Trayan Iliev <office@iproduct.org>
2021-03-26 19:10:45 +0200

    github-promises-demo finished

--- 04-es6-novelties-lab2/github-fetch-promises.js
+++ 04-es6-novelties-lab2/github-fetch-promises.js
@@ -1,7 +1,26 @@
 function demo() {
-    fetch('users.json')
+    const resultsElem = document.getElementById('results');
+    return fetch('users.json')
         .then(userResp => userResp.json())
-        .then(users => console.log(users));
+        .then(users => Promise.all(
+            users.map(user => fetch(`https://api.github.com/users/${user.name}`)
+                .then(gitUserResp => gitUserResp.json())
+            ))
+        ).then(gitUsers => 
+            gitUsers.map(gitUser => {
+                const img = new Image();
+                img.src = gitUser.avatar_url;
+                return img;
+            }).map(userImage => {
+                resultsElem.append(userImage);
+                return userImage;
+            })
+        ).then(images => new Promise((resolve, reject) =>
+            setTimeout(resolve, 6000, images)
+        )).then(images => {
+            images.forEach(image => image.remove());
+            return 'Demo finished';
+        });
 }
 
-window.addEventListener('load', demo)
+window.addEventListener('load', () => demo().then(result => console.log(result)));
