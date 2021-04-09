22c24eb10d8b486a49c1d0b27b44c4dcc5686b85
Trayan Iliev <office@iproduct.org>
2021-04-09 21:07:13 +0300

    todo service added and provided in root injector

---
+++ 07-angular-todos-lab2/src/app/todo-service.service.ts
@@ -0,0 +1,34 @@
+import { Repository, RepositoryImpl, NumberIdGenerator, IdType } from './repository';
+import { Injectable } from '@angular/core';
+import { Todo } from './todo.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoServiceService {
+  private todoRepo: Repository<Todo> = new RepositoryImpl<Todo>(new NumberIdGenerator());
+  constructor() {
+  }
+
+  getAllTodos(): Todo[] {
+    return this.todoRepo.findAll();
+  }
+
+  getTodobyId(todoId: IdType): Todo | undefined {
+    return this.todoRepo.findById(todoId);
+  }
+
+  create(todo: Todo): Todo {
+    return this.todoRepo.create(todo);
+  }
+  update(todo: Todo): Todo | undefined {
+    return this.todoRepo.update(todo);
+  }
+  deleteById(todoId: IdType): Todo | undefined {
+    return this.todoRepo.deleteById(todoId);
+  }
+
+  getTodosCount(): number {
+    return this.todoRepo.count();
+  }
+}
---
+++ 07-angular-todos-lab2/src/app/todo.model.ts
@@ -0,0 +1,9 @@
+import { Identifiable, IdType } from './repository';
+export enum TodoStatus {
+  ACTIVE = 1, COMPLETED, CANCELED
+}
+
+export class Todo implements Identifiable {
+  id: IdType;
+  constructor(public title: string, public status = TodoStatus.ACTIVE){}
+}
