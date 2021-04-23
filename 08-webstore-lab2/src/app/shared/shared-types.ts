24f044952db8eecd4436117bfd7f6ca6e7cfd3b4
Trayan Iliev <office@iproduct.org>
2021-04-23 19:19:50 +0300

    BackendService interface added

--- 08-webstore-lab2/src/app/shared/shared-types.ts
+++ 08-webstore-lab2/src/app/shared/shared-types.ts
@@ -3,3 +3,9 @@ export type IdType = number | undefined;
 export interface Identifiable {
     id: IdType;
 }
+
+export interface ResourceType<T> extends Function {
+  typeId: string;
+  new(...args: any[]): T;
+}
+
