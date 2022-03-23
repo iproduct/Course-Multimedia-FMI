23bcc2bd91c0e5c26479263d376f71f74ca1b96d
Trayan Iliev <office@iproduct.org>
2022-03-23 20:38:45 +0200

    greeter.ts demo updated

--- mmt-2022-04-ts-lab/dist/greeter.js
+++ mmt-2022-04-ts-lab/dist/greeter.js
@@ -1,6 +1,6 @@
-import { UserBase } from "./users";
+import { Role, UserBase } from "./users";
 export function greeter(user) {
     return `${user.salutation} from TypeScript 4.6!`;
 }
-export const user = new UserBase(1, "");
+export const user = new UserBase(1, 'Ivan', 'Petrov', 'ivan@dir.bg', 'ivan123', [Role.READER, Role.AUTHOR, Role.ADMIN]);
 //# sourceMappingURL=greeter.js.map
--- mmt-2022-04-ts-lab/src/greeter.ts
+++ mmt-2022-04-ts-lab/src/greeter.ts
@@ -1,7 +1,7 @@
-import { User, UserBase } from "./users";
+import { Role, User, UserBase } from "./users";
 
 export function greeter(user: User) {
     return `${user.salutation} from TypeScript 4.6!`;
 }
 
-export const user = new UserBase(1, "");
+export const user = new UserBase(1,'Ivan', 'Petrov', 'ivan@dir.bg', 'ivan123', [Role.READER, Role.AUTHOR, Role.ADMIN]);
