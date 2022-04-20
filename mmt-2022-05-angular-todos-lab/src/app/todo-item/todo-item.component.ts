0b0707c1029dc4a7331bedbe349e83ed2ac2da33
Trayan Iliev <office@iproduct.org>
2022-04-20 19:34:47 +0300

    todo-item comp added + global main.css

---
+++ mmt-2022-05-angular-todos-lab/src/app/todo-item/todo-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'td-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
