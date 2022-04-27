04416bb1f818af0953d877feaf054b3d9eb4007b
Trayan Iliev <office@iproduct.org>
2022-04-27 20:19:07 +0300

    webstore project initial commit

---
+++ mmt-2022-06-webstore-lab/src/app/simple-form/simple-form.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { SimpleFormComponent } from './simple-form.component';
+
+
+
+@NgModule({
+  declarations: [
+    SimpleFormComponent
+  ],
+  exports: [],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    FormsModule
+  ]
+})
+export class SimpleFormModule { }
