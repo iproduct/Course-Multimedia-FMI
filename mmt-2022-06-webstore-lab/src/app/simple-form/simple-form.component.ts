8468a49445562e537328c293ccdfe212eaf8cf14
Trayan Iliev <office@iproduct.org>
2022-04-27 21:04:43 +0300

    simple-form + form.css added

--- mmt-2022-06-webstore-lab/src/app/simple-form/simple-form.component.ts
+++ mmt-2022-06-webstore-lab/src/app/simple-form/simple-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'ws-simple-form',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./simple-form.component.scss']
 })
 export class SimpleFormComponent implements OnInit {
+  name = {first: 'John', last: 'Smith'}
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(formModel: NgForm) {
+    console.log(formModel.value);
+    console.log(formModel.valid);
+  }
+
+  setValue() {
+    this.name = {first: 'Brian', last: 'Adams'}
+  }
+
+  // wrapInPromise(value: string) {
+  //   return Promise.resolve(value);
+  // }
 }
