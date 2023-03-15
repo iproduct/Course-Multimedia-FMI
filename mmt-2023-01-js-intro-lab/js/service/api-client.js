4af362506015b54f37b0a2c04714d205163f10b8
Trayan Iliev <office@iproduct.org>
2023-03-15 21:16:31 +0200

    handleRequest refactored

--- mmt-2023-01-js-intro-lab/js/service/api-client.js
+++ mmt-2023-01-js-intro-lab/js/service/api-client.js
@@ -19,4 +19,23 @@ export class ApiClient {
         })
             .then(postResp => postResp.json())
     }
+
+    deleteById(id) {
+        return fetch(`http://localhost:9000/api/${this.collection}/${id}`, {
+            method: 'DELETE'
+        })
+            .then(postResp => postResp.json())
+    }
+
+    async handleRequest(url, options){
+        try {
+            const postsResp = await fetch(url, options);
+            if(postsResp.status >= 400) {
+                return Promise.reject(postsResp.body);
+            }
+            return postsResp.json();
+        } catch(err) {
+            return Promise.reject(err)
+        }
+    }
 }
